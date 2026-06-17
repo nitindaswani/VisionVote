0440a026a73b1aca43af53b76a01da52248eb0d8
nitindaswani <nitindaswani771@gmail.com>
2026-06-17 13:04:47 +0530

    Docker enabled

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+pip install "setuptools<81"
+pip install git+https://github.com/ageitgey/face_recognition_models.git
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@visionvote.com', 'admin123')
+    print('Superuser created.')
+"
